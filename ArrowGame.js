15975e2d411916eb0896c8952d96e8b7a776aeb9
mkcarl <53103933+mkcarl@users.noreply.github.com>
2021-05-19 01:27:44 +0800

    added autosolver for js and solved row colum anomaly

--- ArrowGame.js
+++ ArrowGame.js
@@ -86,13 +86,267 @@ class ArrowBoard{
         return true; 
     }
 
+    // solve(shape, verbose = false){
+    //     if (shape == "4x4"){
+    //         let steps = []; 
+    //         if (this.checkSolved()){
+    //             return "Done";
+    //         } else {
+    //             for (let y=0; y<3; y++){
+    //                 // for the first 3 rows, 
+    //                 let leftCorner = this.board[y][0].state; 
+    //                 let rightCorner = this.board[y][3].state;
+    //                 const update = (board) => {
+    //                     leftCorner = this.board[y][0].state; 
+    //                     rightCorner = this.board[y][3].state;
+    //                 }
+    //                 while (this.board[y][1].state != leftCorner) {
+    //                     // if the left corner piece is different from the adjacent piece, 
+    //                     // turn the piece until it is the same
+    //                     let target = this.board[y+1][2]; 
+    //                     this.rotatePiece(target.position[1], target.position[0]);
+    //                     update(this.board);
+    //                     steps.push(target.position);                       
+    //                     }
+    //                 while (this.board[y][2].state != rightCorner) {
+    //                     let target = this.board[y+1][1];
+    //                     this.rotatePiece(target.position[1], target.position[0]);
+    //                     update(this.board); 
+    //                     steps.push(target.position);                       
+
+    //                 }
+
+    //                 if (leftCorner != 0){
+    //                     // realign the numbers
+    //                     let diff = 4 - leftCorner;
+    //                     for (let i = 0; i < diff; i++) {
+    //                         let target = this.board[y+1][0];                      
+    //                         this.rotatePiece(target.position[1], target.position[0]);
+    //                         update(this.board); 
+    //                         steps.push(target.position);                       
+
+    //                     } 
+    //                 }
+    //                 if (rightCorner != 0){
+    //                     let diff = 4 - rightCorner;
+    //                     for (let i = 0; i < diff; i++) {   
+    //                         let target = this.board[y+1][3];                   
+    //                         this.rotatePiece(target.position[1], target.position[0]);
+    //                         update(this.board); 
+    //                         steps.push(target.position);                       
+
+    //                     } 
+    //                 }
+                    
+                    
+                    
+    //                 if (verbose){
+    //                     let x = this.printBoard(); 
+    //                     console.log(x); 
+    //                 }
+                    
+    //             }
+                
+    //             // left side 
+    //             let centerTile = this.board[3][1]; 
+    //             let adjacentTile = this.board[3][0];
+    //             while (centerTile.state != adjacentTile.state){
+    //                 this.rotatePiece(3, 2); 
+    //                 steps.push(this.board[3][2].position);   
+    //             }
+    //             while (this.board[2][2].state != 0){
+    //                 this.rotatePiece(1,2); 
+    //                 steps.push(this.board[1][2].position);
+    //             }
+    //             while (this.board[0][2].state != 0){
+    //                 this.rotatePiece(0,2); 
+    //                 steps.push(this.board[0][2].position); 
+    //             }
+
+    //             // right side
+    //             centerTile = this.board[3][2]; 
+    //             adjacentTile = this.board[3][3];
+    //             while (centerTile.state != adjacentTile.state){
+    //                 this.rotatePiece(3, 1); 
+    //                 steps.push(this.board[3][1].position);   
+    //             }
+    //             while (this.board[2][1].state != 0){
+    //                 this.rotatePiece(1,1); 
+    //                 steps.push(this.board[1][1].position);
+    //             }
+    //             while (this.board[0][1].state != 0){
+    //                 this.rotatePiece(0,1); 
+    //                 steps.push(this.board[0][1].position); 
+    //             }
+
+    //             // change left pair 
+    //             while (this.board[3][0].state != 0){
+    //                 this.rotatePiece(3, 0); 
+    //                 steps.push(this.board[3][0].position);
+    //             }
+    //             while (this.board[2][0].state != 0){
+    //                 this.rotatePiece(1, 0); 
+    //                 steps.push(this.board[1][0].position); 
+    //             }
+    //             while (this.board[0][0].state != 0) {
+    //                 this.rotatePiece(0,0); 
+    //                 steps.push(this.board[0][0].position);
+    //             }
+
+    //             // change right pair 
+    //             while (this.board[3][3].state != 0){
+    //                 this.rotatePiece(3, 3); 
+    //                 steps.push(this.board[3][3].position);
+    //             }
+    //             while (this.board[2][3].state != 0){
+    //                 this.rotatePiece(1, 3); 
+    //                 steps.push(this.board[1][3].position); 
+    //             }
+    //             while (this.board[0][3].state != 0) {
+    //                 this.rotatePiece(0,3); 
+    //                 steps.push(this.board[0][3].position);
+    //             }
+    //         }
+            
+    //         return steps; 
+    //     }
+    // }
 }
 
+class ArrowBoardSolver{
+    constructor(board){
+        this.arrowBoard = board; 
+    }
+    solve(shape, verbose = false){
+        if (shape == "4x4"){
+            let steps = []; 
+            if (this.arrowBoard.checkSolved()){
+                return "Done";
+            } else {
+                for (let y=0; y<3; y++){
+                    // for the first 3 rows, 
+                    let leftCorner = this.arrowBoard.board[y][0].state; 
+                    let rightCorner = this.arrowBoard.board[y][3].state;
+                    const update = (board) => {
+                        leftCorner = this.arrowBoard.board[y][0].state; 
+                        rightCorner = this.arrowBoard.board[y][3].state;
+                    }
+                    while (this.arrowBoard.board[y][1].state != leftCorner) {
+                        // if the left corner piece is different from the adjacent piece, 
+                        // turn the piece until it is the same
+                        let target = this.arrowBoard.board[y+1][2]; 
+                        this.arrowBoard.rotatePiece(target.position[1], target.position[0]);
+                        update(this.arrowBoard.board);
+                        steps.push(target.position);                       
+                        }
+                    while (this.arrowBoard.board[y][2].state != rightCorner) {
+                        let target = this.arrowBoard.board[y+1][1];
+                        this.arrowBoard.rotatePiece(target.position[1], target.position[0]);
+                        update(this.arrowBoard.board); 
+                        steps.push(target.position);                       
 
+                    }
 
+                    if (leftCorner != 0){
+                        // realign the numbers
+                        let diff = 4 - leftCorner;
+                        for (let i = 0; i < diff; i++) {
+                            let target = this.arrowBoard.board[y+1][0];                      
+                            this.arrowBoard.rotatePiece(target.position[1], target.position[0]);
+                            update(this.arrowBoard.board); 
+                            steps.push(target.position);                       
 
-// b = new ArrowBoard(3, 3, 4); 
+                        } 
+                    }
+                    if (rightCorner != 0){
+                        let diff = 4 - rightCorner;
+                        for (let i = 0; i < diff; i++) {   
+                            let target = this.arrowBoard.board[y+1][3];                   
+                            this.arrowBoard.rotatePiece(target.position[1], target.position[0]);
+                            update(this.arrowBoard.board); 
+                            steps.push(target.position);                       
 
+                        } 
+                    }
+                    
+                    
+                    
+                    if (verbose){
+                        let x = this.arrowBoard.printBoard(); 
+                        console.log(x); 
+                    }
+                    
+                }
+                
+                // left side 
+                let centerTile = this.arrowBoard.board[3][1]; 
+                let adjacentTile = this.arrowBoard.board[3][0];
+                while (centerTile.state != adjacentTile.state){
+                    this.arrowBoard.rotatePiece(3, 2); 
+                    steps.push(this.arrowBoard.board[3][2].position);   
+                }
+                while (this.arrowBoard.board[2][2].state != 0){
+                    this.arrowBoard.rotatePiece(1,2); 
+                    steps.push(this.arrowBoard.board[1][2].position);
+                }
+                while (this.arrowBoard.board[0][2].state != 0){
+                    this.arrowBoard.rotatePiece(0,2); 
+                    steps.push(this.arrowBoard.board[0][2].position); 
+                }
+
+                // right side
+                centerTile = this.arrowBoard.board[3][2]; 
+                adjacentTile = this.arrowBoard.board[3][3];
+                while (centerTile.state != adjacentTile.state){
+                    this.arrowBoard.rotatePiece(3, 1); 
+                    steps.push(this.arrowBoard.board[3][1].position);   
+                }
+                while (this.arrowBoard.board[2][1].state != 0){
+                    this.arrowBoard.rotatePiece(1,1); 
+                    steps.push(this.arrowBoard.board[1][1].position);
+                }
+                while (this.arrowBoard.board[0][1].state != 0){
+                    this.arrowBoard.rotatePiece(0,1); 
+                    steps.push(this.arrowBoard.board[0][1].position); 
+                }
+
+                // change left pair 
+                while (this.arrowBoard.board[3][0].state != 0){
+                    this.arrowBoard.rotatePiece(3, 0); 
+                    steps.push(this.arrowBoard.board[3][0].position);
+                }
+                while (this.arrowBoard.board[2][0].state != 0){
+                    this.arrowBoard.rotatePiece(1, 0); 
+                    steps.push(this.arrowBoard.board[1][0].position); 
+                }
+                while (this.arrowBoard.board[0][0].state != 0) {
+                    this.arrowBoard.rotatePiece(0,0); 
+                    steps.push(this.arrowBoard.board[0][0].position);
+                }
+
+                // change right pair 
+                while (this.arrowBoard.board[3][3].state != 0){
+                    this.arrowBoard.rotatePiece(3, 3); 
+                    steps.push(this.arrowBoard.board[3][3].position);
+                }
+                while (this.arrowBoard.board[2][3].state != 0){
+                    this.arrowBoard.rotatePiece(1, 3); 
+                    steps.push(this.arrowBoard.board[1][3].position); 
+                }
+                while (this.arrowBoard.board[0][3].state != 0) {
+                    this.arrowBoard.rotatePiece(0,3); 
+                    steps.push(this.arrowBoard.board[0][3].position);
+                }
+            }
+            
+            return steps; 
+        }
+    }
+}
+
+// let b = new ArrowBoard(4,4); 
+// let steps = b.solve("4x4", true); 
+// console.log(steps); 
 // while (true) {
 //     console.log(b.printBoard()); 
 //     if (b.checkSolved()){
--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-const WIDTH = 600; 
-const HEIGHT = 600; 
+const WIDTH = 800; 
+const HEIGHT = 800; 
 const MARGIN = 50
-const DIM = [3,3]; 
+const DIM = [4,4]; 
 let boardSize = [WIDTH-2*MARGIN, HEIGHT-2*MARGIN]; 
-let board = new ArrowBoard(3,3);
+let board = new ArrowBoard(DIM[0], DIM[1]);
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
@@ -16,13 +16,13 @@ function draw() {
 }
 
 function mouseClicked(){
-  // console.log(`[${mouseX},${mouseY}]`)
   if (mouseX > MARGIN && mouseX < boardSize[0]+MARGIN && mouseY > MARGIN && mouseY < boardSize[1]+MARGIN && !board.checkSolved()){
     // console.log(`${Math.floor(mouseX/(boardSize[0]/DIM[0]))}`)
-    let x = Math.floor(mouseX/(boardSize[0]/DIM[0]+MARGIN)); 
-    let y = Math.floor(mouseY/(boardSize[1]/DIM[1]+MARGIN)); 
+    let x = Math.floor((mouseX-MARGIN)/(boardSize[0]/DIM[0])); 
+    let y = Math.floor((mouseY-MARGIN)/(boardSize[1]/DIM[1])); 
     console.log(x, y)
-    board.rotatePiece(x, y)
+    console.log(`[${mouseX},${mouseY}]`)
+    board.rotatePiece(y, x)
     drawBoard();
     
   }
@@ -32,11 +32,12 @@ function mouseClicked(){
 }
 
 function drawBoard(){
+  console.log(board.printBoard()); 
   rect(MARGIN, MARGIN, boardSize[0], boardSize[1]);
   
   // drawing the grid
-    for (let x = 1; x < DIM[0]; x++) {
-        for (let y = 1; y < DIM[1]; y++) {
+  for (let x = 1; x < DIM[0]; x++) {
+    for (let y = 1; y < DIM[1]; y++) {
             stroke(0); 
             strokeWeight(1); 
             line(x * boardSize[0]/DIM[0]+MARGIN, MARGIN, x * boardSize[0]/DIM[0]+MARGIN, boardSize[0]+MARGIN)
@@ -45,9 +46,9 @@ function drawBoard(){
     }
     textSize((boardSize[0]/DIM[0] + boardSize[1]/DIM[1])/4);
     textAlign("center", "center") 
-    for (let x = 1; x <= DIM[0]; x++) {
-      for (let y = 1; y <= DIM[1]; y++) {
-        text(`${board.board[x-1][y-1].state}`, 
+    for (let y = 1; y <= DIM[1]; y++) {
+      for (let x = 1; x <= DIM[0]; x++) {
+        text(`${board.board[y-1][x-1].state}`, 
         x * boardSize[0]/DIM[0]+MARGIN - boardSize[0]/DIM[0]/2, 
         y * boardSize[1]/DIM[1]+MARGIN - boardSize[1]/DIM[1]/2)
       }
